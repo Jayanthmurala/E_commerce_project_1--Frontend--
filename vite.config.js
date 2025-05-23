bacf1ae52827376f006d2f86ae36d527f7828131
Jayanthmurala <Jayanthmurala@gmail.com>
2025-05-24 03:52:38 +0530

    v1.1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// Safely handle PORT
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+console.log("Using port:", PORT);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: PORT,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: PORT,
+  },
+  build: {
+    outDir: "dist",
+    minify: "esbuild",
+    sourcemap: false,
+  },
 });
